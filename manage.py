2aacd8bdd2258896731a4707fb80298cb64f495c
erose357 <erose357@gmail.com>
2017-12-27 15:54:38 -0700

    Adds data files to project

--- manage.py
+++ manage.py
@@ -11,5 +11,9 @@ manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
 
+@manager.command
+def seed():
+    print("seed test")
+
 if __name__ == '__main__':
     manager.run()
---
+++ test/test_flask_engine.py
@@ -0,0 +1,6 @@
+import unittest
+import os
+import json
+from app import create_app, db
+
+class MerchantsTest(unittest.TestCase):
