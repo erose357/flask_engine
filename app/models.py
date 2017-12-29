1f23ebf0391f79f8f4bbd4cc613a43a867f42a6f
erose357 <erose357@gmail.com>
2017-12-29 15:00:33 -0700

    Adds migration for items table

--- app/models.py
+++ app/models.py
@@ -33,4 +33,16 @@ class Customers(db.Model):
     def __repr__(self):
         return "<Customers: {}>".format(self.name)
 
+class Items(db.Model):
+    __tablename__ = 'items'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+    description = db.Column(db.Text)
+    unit_price = db.Column(db.Integer)
+    merchant_id = db.Column(db.Integer, db.ForeignKey('merchants.id'))
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    updated_at = db.Column(
+            db.DateTime, default=db.func.current_timestamp(),
+            onupdate=db.func.current_timestamp())
+
 
